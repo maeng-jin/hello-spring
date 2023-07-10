1bf53f825da267dfe1a7e32ad327c8db9d0e5a22
박명진 <myungjin@danbiedu.co.kr>
2023-07-11 01:45:48 +0900

    springdoc(swagger-ui) 추가

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,35 +1,41 @@
 package hello.hellospring.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Tag(name = "스프링 입문", description = "코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술")
 @Controller
 public class HelloController {
 
+    @Operation(summary = "summary", description = "description")
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello";
     }
 
+    @Operation(summary = "summary", description = "description")
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
     }
 
+    @Operation(summary = "summary", description = "description")
     @GetMapping("hello-string")
     @ResponseBody
     public String helloString(@RequestParam(value = "name", required = false) String name) {
         return "hello " + name;
     }
 
+    @Operation(summary = "summary", description = "description")
     @GetMapping("hello-api")
     @ResponseBody
     public Hello helloApi(@RequestParam(value = "name", required = false) String name) {
