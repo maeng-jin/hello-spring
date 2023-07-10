7e97c8ad4cd84374b737c0623bc1b2b8c8b697ec
박명진 <myungjin@danbiedu.co.kr>
2023-07-11 01:16:28 +0900

    섹션 2 - 스프링 웹 개발 기초 | API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,13 @@
 package hello.hellospring.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +22,25 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name", required = false) String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name", required = false) String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    static class Hello {
+        private String name;
     }
 }
