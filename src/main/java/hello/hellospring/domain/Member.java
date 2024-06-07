6cbfc5600b671182b457f0704cfde3e1b8cfb556
박명진 <myungjin@danbiedu.co.kr>
2024-06-07 11:31:38 +0900

    섹션 3 - 회원 관리 예제 - 백엔드 개발 | 회원 서비스 개발

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -3,9 +3,11 @@ package hello.hellospring.domain;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Member {
     @Schema(description = "아이디", defaultValue = "1")
     private Long id;
